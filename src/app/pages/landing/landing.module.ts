5965cab337d0b16e16a2280d7983c40b31d3c22a
kingsley-people-box <kingsley@peoplebox.ai>
2023-01-03 10:03:58 +0100

    add product, header components

---
+++ src/app/pages/landing/landing.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from "@angular/core";
+import { LandingComponent } from "./landing.component";
+
+@NgModule({
+  declarations: [LandingComponent],
+  exports: [LandingComponent],
+})
+export class LandingModule {}
